7e79a5b0f99710570342b6c1ba5e73cc130edb64
Jonathanimo <jonathanimo89@gmail.com>
2015-05-26 22:02:46 -0500

    changes to routes to add races route and candidate route  TODO:fix post route to candidates

--- builds/development/js/controllers/status.js
+++
@@ -1,9 +0,0 @@
-elexApp.controller('StatusController', function($scope, 
-  $location, Authentication) {
-
-  $scope.logout = function() {
-    Authentication.logout();
-    $location.path('/login');
-  }; //logout
-
-}); //StatusController
--- routes/index.js
+++ routes/index.js
@@ -11,6 +11,27 @@ var Candidate = mongoose.model('Candidate');
 var Race = mongoose.model('Race');
 var Election = mongoose.model('Election');
 
+
+router.param('election', function(req, res, next, id) {
+  var query = Election.findById(id);
+  query.exec(function (err, election){
+    if (err) { return next(err); }
+    if (!election) { return next(new Error('can\'t find election')); }
+    req.election = election;
+    return next();
+  });
+});
+
+router.param('race', function(req, res, next, id) {
+  var query = Race.findById(id);
+  query.exec(function (err, race){
+    if (err) { return next(err); }
+    if (!race) { return next(new Error('can\'t find race')); }
+    req.race = race;
+    return next();
+  });
+});
+
 router.get('/election', function(req, res, next) {
 	Election.find(function(err, elections){
     	if(err){ 
@@ -22,7 +43,6 @@ router.get('/election', function(req, res, next) {
 
 router.post('/election', function(req, res, next) {
   var election = new Election(req.body);
-
   election.save(function(err, election){
     if(err){ return next(err); }
 
@@ -30,35 +50,43 @@ router.post('/election', function(req, res, next) {
   });
 });
 
-router.param('election', function(req, res, next, id) {
-  var query = Election.findById(id);
-
-  query.exec(function (err, election){
+router.get('/election/:election', function(req, res) {
+   req.election.populate('races', function(err, election) {
     if (err) { return next(err); }
-    if (!election) { return next(new Error('can\'t find election')); }
-
-    req.election = election;
-    return next();
+    res.json(req.election);
   });
 });
 
-router.get('/election/:election', function(req, res) {
-  res.json(req.election);
+router.get('/election/:election/race/:race', function(req, res) {
+    res.json(req.election.race);
 });
 
-router.post('/election/:election/races', function(req, res, next) {
+router.post('/election/:election/race', function(req, res, next) {
   var race = new Race(req.body);
-  race.post = req.post;
-
+  race.election = req.election;
   race.save(function(err, race){
     if(err){ return next(err); }
-
-    req.post.races.push(race);
-    req.post.save(function(err, election) {
+    req.election.races.push(race);
+    req.election.save(function(err, election) {
       if(err){ return next(err); }
       res.json(race);
     });
   });
 });
+
+router.post('/election/:election/race/:race/candidate', function(req, res, next) {
+  var candidate = new Candidate(req.body);
+  candidate.race = req.election.race;
+  candidate.save(function(err, candidate){
+    if(err){ return next(err); }
+    req.election.races.race.candidates.push(candidate);
+    req.race.save(function(err, race) {
+      if(err){ return next(err); }
+      res.json(candidate);
+    });
+  });
+});
+
+
 
 module.exports = router;
